e9158bfb4eeb0bb000136ac9ec8106dfb199ef4a
Leonardo <leonardo@Lenovo-B470.(none)>
2013-10-01 16:22:00 -0430

    repuestos

---
+++ repuesto/repo/Repuestos/manager/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from manager.models import *
+
+admin.site.register(Vehiculo)
+admin.site.register(Repuesto)
+admin.site.register(Pedido)
+admin.site.register(Telefono_Clientes)
+
