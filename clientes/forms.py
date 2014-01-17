3769067fcedd192689557fa9ae2fbcc807756ca3
leonardo <leonardogutierrezh@gmail.com>
2014-01-17 17:03:16 -0430

    formulario macroclientes

---
+++ clientes/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+from django.db import models
+from clientes.models import *
+from django import forms
+
+class MacroClienteForm(forms.ModelForm):
+    class Meta:
+        model = MacroCliente
+        
