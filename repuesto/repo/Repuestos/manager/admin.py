4e7ee4bdaef14a5152d8ed7b3b5f463345490e1c
leonardo <leonardogutierrezh@gmail.com>
2014-01-17 15:53:20 -0430

    quitar repuesto

--- repuesto/repo/Repuestos/manager/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from manager.models import *
-
-admin.site.register(Vehiculo)
-admin.site.register(Repuesto)
-admin.site.register(Pedido)
-admin.site.register(Telefono_Clientes)
-
