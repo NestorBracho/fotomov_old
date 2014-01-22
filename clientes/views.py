ca7ebf08c9565e8fb80d07d556146d11e37c9107
leonardo <leonardogutierrezh@gmail.com>
2014-01-22 18:49:57 -0430

    avance de direcciones en macroclientes

--- clientes/views.py
+++ clientes/views.py
@@ -12,8 +12,15 @@ def nuevo_macrocliente(request):
     if request.method == 'POST':
         formulario = MacroClienteForm(request.POST)
         if formulario.is_valid():
+
             print "valido"
             macrocliente = formulario.save()
+            direcciones = request.POST.getlist('dir')
+            i = 0
+            while i < len(direcciones):
+                direccion = Direccion.objects.create(macrocliente=macrocliente, direccion=direcciones[i], lat=direcciones[i+2], lon=direcciones[i+3], descripcion=direcciones[i+1])
+                direccion.save()
+                i += 4
             print macrocliente.nombre
             return HttpResponseRedirect('/listar_macroclientes/1')
     else:
@@ -43,14 +50,21 @@ def editar_macrocliente(request, id_macrocliente):
             return HttpResponseRedirect('/listar_macroclientes/2')
     else:
         formulario = MacroClienteForm(initial={'submarca': macrocliente.submarca, 'nombre': macrocliente.nombre, 'telefono': macrocliente.telefono, 'rif': macrocliente.rif, 'direccion_fiscal': macrocliente.direccion_fiscal, 'descripcion': macrocliente.descripcion})
-    return render_to_response('clientes/nuevo_macrocliente.html', {'formulario': formulario}, context_instance = RequestContext(request))
+    return render_to_response('clientes/editar_macrocliente.html', {'formulario': formulario}, context_instance = RequestContext(request))
 
 def ver_macrocliente(request, id_macrocliente):
     if MacroCliente.objects.filter(id = id_macrocliente):
         macrocliente = MacroCliente.objects.get(id = id_macrocliente)
+        direcciones = Direccion.objects.filter(macrocliente=macrocliente)
+        if len(direcciones) > 0:
+            tienedir = True
+            primeraDir = direcciones[0]
+        else:
+            tienedir = False
+            primeraDir = None
     else:
         return HttpResponseRedirect('/listar_macroclientes/0')
-    return render_to_response('clientes/ver_macrocliente.html', {'macrocliente': macrocliente}, context_instance = RequestContext(request))
+    return render_to_response('clientes/ver_macrocliente.html', {'macrocliente': macrocliente, 'tienedir': tienedir, 'primeraDir': primeraDir, 'direcciones': direcciones}, context_instance = RequestContext(request))
 
 def eliminar_macrocliente(request, id_macrocliente):
     if MacroCliente.objects.filter(id=id_macrocliente):
--- staff/admin.py
+++ staff/admin.py
@@ -1,6 +1,9 @@
 from staff.models import Usuario
 from django.contrib import admin
 from staff.models import *
+from clientes.models import *
 
 admin.site.register(Usuario)
 admin.site.register(Privilegios)
+admin.site.register(Direccion)
+admin.site.register(MacroCliente)
