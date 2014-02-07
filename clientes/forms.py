df4cc10edd0eff217eed4f22bf91d7e8818db3bc
nestorbracho <nestorbracho2207@gmail.com>
2014-02-07 14:17:20 -0430

    macrocliente ajax final

--- clientes/forms.py
+++ clientes/forms.py
@@ -12,6 +12,7 @@ class MacroClienteForm(forms.ModelForm):
 class MacroClienteContactoForm(forms.Form):
     nombre = forms.CharField(max_length=100)
     cedula = forms.CharField(max_length=8)
+    cargo = forms.CharField(max_length=30)
     telefono = forms.CharField(max_length=11)
     email = forms.EmailField()
     descripcion = forms.CharField(widget=forms.Textarea,max_length=500)
