99112b2eb8e89c42d592742a23af0d27f45c98f1
leonardo <leonardogutierrezh@gmail.com>
2014-02-11 16:08:20 -0430

    guarda direcciones

--- direcciones/views.py
+++ direcciones/views.py
@@ -6,11 +6,23 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext, loader, Context, Template
 from django.contrib.auth.decorators import login_required
 from django.core import serializers
-from direcciones import *
+from direcciones.models import *
 
 def nueva_direccion(request):
+    direcciones = Direccion.objects.all()
     if request.method == 'POST':
-        pass
+        latlng = request.POST.get('latlng').split(',')
+        descripcion = request.POST.get('descripcion')
+        nombre = request.POST.get('nombre')
+        print nombre
+        direccion = request.POST.get('direccion')
+        lat=float(latlng[0])
+        print latlng[0]
+        lng =float(latlng[1])
+        print lng
+        dir = Direccion.objects.create(nombre=nombre, direccion=direccion, lat=float(lat), lon=float(lng),descripcion=descripcion)
+        dir.save()
+
     else:
         pass
-    return render_to_response('staff/nueva_direccion.html', {}, context_instance= RequestContext(request))
+    return render_to_response('staff/nueva_direccion.html', {'direcciones': direcciones}, context_instance= RequestContext(request))
