e71e250af9b8bd6a32750df87c424a52bb8c22ff
leonardo <leonardogutierrezh@gmail.com>
2014-02-18 16:08:59 -0430

    funcion onclick en tr de direccion

--- productos/views.py
+++ productos/views.py
@@ -1,1 +1,26 @@
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext, loader, Context, Template
+from django.contrib.auth.decorators import login_required
+from django.core import serializers
+from productos.models import *
+
+def nuevo_producto(request):
+    if request.method == 'POST':
+        pass
+    else:
+        pass
+    return render_to_response('productos/nuevo_producto.html', {}, context_instance=RequestContext(request))
+
+def editar_producto(request):
+    if request.method == 'POST':
+        pass
+    else:
+        pass
+    return render_to_response('productos/nuevo_producto.html', {}, context_instance=RequestContext(request))
+
+def eliminar_producto(request):
+    return True
