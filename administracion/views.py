f5a099e9a8c6ac595d573ea43d34a4d51c5c5489
nestorbracho <nestorbracho2207@gmail.com>
2014-03-26 16:37:47 -0430

    recursividad de tareas

--- administracion/views.py
+++ administracion/views.py
@@ -39,16 +39,16 @@ def nuevo_tipo_de_gasto(request):
     return render_to_response('administracion/nuevo_tipo_de_gasto.html', {'formulario': formulario}, context_instance = RequestContext(request))
 
 def nuevo_gasto(request):
-    today = datetime.datetime.today()
+    #today = datetime.datetime.today()
     #print (today.strftime('%A'))
     #print today
-    d = datetime.datetime.today().year
-    d= today + datetime.timedelta(days = 1)
+    #d = datetime.datetime.today().year
+    #d= today + datetime.timedelta(days = 1)
     #print (d.strftime('%A'))
-    d = (date(1900,4,28).strftime('%A'))
+    #d = (date(1900,4,28).strftime('%A'))
     #print d
-    d = date(datetime.datetime.today().year,4,29)
-    d = d + datetime.timedelta(days=3, milliseconds=4)
+    #d = date(datetime.datetime.today().year,4,29)
+    #d = d + datetime.timedelta(days=3, milliseconds=4)
     #print d
     #
     hoy = datetime.datetime.today()
@@ -56,7 +56,7 @@ def nuevo_gasto(request):
         formulario = GastoForm(request.POST)
         if formulario.is_valid():
             gasto = formulario.save()
-            tarea = formulario.cleaned_data['tipo'].nombre+' '+formulario.cleaned_data['nombre']+', '+str(formulario.cleaned_data['monto'])+' en '+formulario.cleaned_data['forma_de_pago'].nombre
+            tarea = formulario.cleaned_data['tipo'].nombre+' '+formulario.cleaned_data['nombre']+', '+str(formulario.cleaned_data['monto'])+' Bs. en '+formulario.cleaned_data['forma_de_pago'].nombre
             if formulario.cleaned_data['frecuencia'] == 1:
                 if formulario.cleaned_data['intervalos_dias'] == 1:
                     dia = "Monday"
@@ -75,41 +75,41 @@ def nuevo_gasto(request):
                 while (hoy.strftime('%A')) != dia:
                     hoy = hoy + datetime.timedelta(days = 1)
                 fech_act = hoy - datetime.timedelta(days = 1)
-                Tarea.objects.create(asignado = Privilegios.objects.get(valor = 5), nombre = formulario.cleaned_data['nombre'], tarea = tarea, es_periodica = True, original = gasto.id, fecha = hoy, fecha_activacion = fech_act)
+                Tarea.objects.create(asignado = Privilegios.objects.get(valor = 5), nombre = formulario.cleaned_data['nombre'], tarea = tarea, es_periodica = True, original = gasto.id, fecha = hoy, fecha_activacion = fech_act, lista = 'False', activa = True)
             elif formulario.cleaned_data['frecuencia'] == 2:
                 if hoy.day <= formulario.cleaned_data['intervalos_dias']:
                     hoy = date(hoy.year,hoy.month,formulario.cleaned_data['intervalos_dias'])
                     fech_act = hoy - datetime.timedelta(days = 1)
-                    Tarea.objects.create(asignado = Privilegios.objects.get(valor = 5), nombre = formulario.cleaned_data['nombre'], tarea = tarea, es_periodica = True, original = gasto.id, fecha = hoy, fecha_activacion = fech_act)
+                    Tarea.objects.create(asignado = Privilegios.objects.get(valor = 5), nombre = formulario.cleaned_data['nombre'], tarea = tarea, es_periodica = True, original = gasto.id, fecha = hoy, fecha_activacion = fech_act, lista = 'False', activa = True)
                 else:
                     dtarea = date(hoy.year,hoy.month,formulario.cleaned_data['intervalos_dias'])
                     dtarea = dtarea + datetime.timedelta(days = 15)
                     if hoy.day <= dtarea.day:
-                        if formulario.cleaned_data['intervalos_dias'] == 30:
+                        if dtarea.day == 30:
                             dtarea = date(hoy.year,(hoy.month+1),1)
                             dtarea = dtarea - datetime.timedelta(days = 1)
                             fech_act = dtarea - datetime.timedelta(days = 1)
-                            Tarea.objects.create(asignado = Privilegios.objects.get(valor = 5), nombre = formulario.cleaned_data['nombre'], tarea = tarea, es_periodica = True, original = gasto.id, fecha = dtarea, fecha_activacion = fech_act)
+                            Tarea.objects.create(asignado = Privilegios.objects.get(valor = 5), nombre = formulario.cleaned_data['nombre'], tarea = tarea, es_periodica = True, original = gasto.id, fecha = dtarea, fecha_activacion = fech_act, lista = 'False', activa = True)
                         else:
                             fech_act = dtarea - datetime.timedelta(days = 1)
-                            Tarea.objects.create(asignado = Privilegios.objects.get(valor = 5), nombre = formulario.cleaned_data['nombre'], tarea = tarea, es_periodica = True, original = gasto.id, fecha = dtarea, fecha_activacion = fech_act)
+                            Tarea.objects.create(asignado = Privilegios.objects.get(valor = 5), nombre = formulario.cleaned_data['nombre'], tarea = tarea, es_periodica = True, original = gasto.id, fecha = dtarea, fecha_activacion = fech_act, lista = 'False', activa = True)
                     else:
                         dtarea = date(hoy.year,hoy.month,formulario.cleaned_data['intervalos_dias'])
                         aux = dtarea.day
                         dtarea = dtarea + datetime.timedelta(days = 31)
                         dtarea = date(dtarea.year,dtarea.month, aux)
                         fech_act = dtarea - datetime.timedelta(days = 1)
-                        Tarea.objects.create(asignado = Privilegios.objects.get(valor = 5), nombre = formulario.cleaned_data['nombre'], tarea = tarea, es_periodica = True, original = gasto.id, fecha = dtarea, fecha_activacion = fech_act)
+                        Tarea.objects.create(asignado = Privilegios.objects.get(valor = 5), nombre = formulario.cleaned_data['nombre'], tarea = tarea, es_periodica = True, original = gasto.id, fecha = dtarea, fecha_activacion = fech_act, lista = 'False', activa = True)
             elif formulario.cleaned_data['frecuencia'] == 3:
                 if formulario.cleaned_data['intervalos_dias'] == 31:
                     dtarea = date(hoy.year,(hoy.month+1),1)
                     dtarea = dtarea - datetime.timedelta(days = 1)
                     fech_act = dtarea - datetime.timedelta(days = 1)
-                    Tarea.objects.create(asignado = Privilegios.objects.get(valor = 5), nombre = formulario.cleaned_data['nombre'], tarea = tarea, es_periodica = True, original = gasto.id, fecha = dtarea, fecha_activacion = fech_act)
+                    Tarea.objects.create(asignado = Privilegios.objects.get(valor = 5), nombre = formulario.cleaned_data['nombre'], tarea = tarea, es_periodica = True, original = gasto.id, fecha = dtarea, fecha_activacion = fech_act, lista = 'False', activa = True)
                 else:
                     dtarea = date(hoy.year,hoy.month,formulario.cleaned_data['intervalos_dias'])
                     fech_act = dtarea - datetime.timedelta(days = 1)
-                    Tarea.objects.create(asignado = Privilegios.objects.get(valor = 5), nombre = formulario.cleaned_data['nombre'], tarea = tarea, es_periodica = True, original = gasto.id, fecha = dtarea, fecha_activacion = fech_act)
+                    Tarea.objects.create(asignado = Privilegios.objects.get(valor = 5), nombre = formulario.cleaned_data['nombre'], tarea = tarea, es_periodica = True, original = gasto.id, fecha = dtarea, fecha_activacion = fech_act, lista = 'False', activa = True)
     else:
         formulario = GastoForm()
     return render_to_response('administracion/nuevo_gasto.html', {'formulario': formulario}, context_instance = RequestContext(request))
