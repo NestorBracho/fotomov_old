99307e882bd3c368946118ba5d680daca6ac7e75
leonardo <leonardogutierrezh@gmail.com>
2014-03-31 19:00:21 -0430

    navegacion en el modulo movil casi lista

--- modulo_movil/models.py
+++ modulo_movil/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from evento.models import Funcion
+from django.contrib.auth.models import User
 
 class direccionFuncion(models.Model):
     funcion = models.ForeignKey(Funcion)
@@ -7,3 +8,7 @@ class direccionFuncion(models.Model):
 
 class direcciones_exportacion(models.Model):
     ruta = models.CharField(max_length=1000)
+
+class directorio_actual(models.Model):
+    usuario = models.ForeignKey(User)
+    directorio = models.CharField(max_length=3000)
