642059fec489056192cb5035c965edce628fbf72
nestorbracho <nestorbracho2207@gmail.com>
2014-03-31 19:14:13 -0430

    clases de dias alertas de tareas

--- fotomov/context_processors.py
+++ fotomov/context_processors.py
@@ -1,16 +1,32 @@
 from staff.models import *
 from tareas.models import *
 from django.conf import settings
+from datetime import *
+import datetime
 
 def obtener_tareas(request):
     print settings.MEDIA_ROOT
-    tareas = ""
     if request.user:
         if request.user.is_authenticated():
             us = request.user
             try:
+                hoy = date(datetime.datetime.today().year,datetime.datetime.today().month,datetime.datetime.today().day)
                 usuario = Usuario.objects.get(usuario=us)
                 tareas = Tarea.objects.filter(asignado=usuario.privilegio, activa=True)
+                muchos_stats = []
+                for tarea in tareas:
+                    if tarea.fecha == hoy:
+                        aux = '2-'+str(tarea.id)
+                        muchos_stats.append(aux)    #la tarea es hoy!
+                    elif hoy + datetime.timedelta(days = 1) == tarea.fecha:
+                        aux = '1-'+str(tarea.id)
+                        muchos_stats.append(aux)    #la tarea es maniana!
+                    elif tarea.fecha < hoy:
+                        aux = '3-'+str(tarea.id)
+                        muchos_stats.append(aux)    #la tarea esta vencida
+                    else:
+                        aux = '0-'+str(tarea.id)
+                        muchos_stats.append(aux)    #no hay rollo
             except:
                 pass
-    return { 'tareas_menu': tareas }
+    return { 'tareas_menu': tareas, 'stats':muchos_stats }
